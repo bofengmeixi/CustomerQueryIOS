643114ae12486962e23b82b2c7d55306ed4671e6
apple <15010347239@163.com>
2018-12-03 17:29:44 +0800

    创建工程

---
+++ CustomerQuery/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CustomerQuery
+//
+//  Created by apple on 2018/12/3.
+//  Copyright © 2018 诸葛云游. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CustomerQuery/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CustomerQuery
+//
+//  Created by apple on 2018/12/3.
+//  Copyright © 2018 诸葛云游. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
